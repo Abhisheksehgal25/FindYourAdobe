5b657dc8d88b4758ebff0be33be1a4348e319d8e
Abhisheksehgal25 <90157277+Abhisheksehgal25@users.noreply.github.com>
2022-01-29 21:41:30 +0530

    Add files via upload

---
+++ FYA/proj.js
@@ -0,0 +1,58 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1000); // Change image every 1 seconds
+}
+
+
+function checkPassword(form) { 
+//to validate name
+var regex = /^[a-zA-Z]+$/;
+    if(regex.test(form.yourname.value) == false){
+   alert("Name must be in alphabets only");
+   form.yourname.focus();
+   return false;
+    }
+    
+                password1 = form.password1.value; 
+                password2 = form.password2.value; 
+  
+                // If password not entered 
+                if (password1 == '') 
+                    alert ("Please enter Password"); 
+                      
+                // If confirm password not entered 
+                else if (password2 == '') 
+                    alert ("Please enter confirm password"); 
+                      
+                // If Not same return False.     
+                else if (password1 != password2) { 
+                    alert ("\nPassword did not match: Please try again...") 
+                    return false; 
+                } 
+  
+                // If same return True. 
+                else{
+				
+                    alert("Password Match: Welcome!") 
+                    return true; 
+                } 
+				
+            } 
+		
+
+
